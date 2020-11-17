435bf570148e7d91c6af6626bfbe0ceb6eac8789
Lorenz Minder <lminder@gmx.net>
2020-11-16 23:23:11 -0600

    build:  Disable BUILD_DOCS by default.
    Building with documentation adds the dependency on Doxygen.  It also
    creates a slew of warnings currently, due to incomplete documentation.
    This can hide other problems, and is also distracting.  Once there are less
    warnings, we can consider if we want to reenable this by default.
    
    While here, I notice that the BUILD_DOCS option was unintentionally added
    twice.

--- cmake/DoxySetup.cmake
+++ cmake/DoxySetup.cmake
@@ -1,6 +1,6 @@
 # Prepare building the documentation
 
-option(BUILD_DOCS "Build documentation" ON)
+option(BUILD_DOCS "Build documentation" OFF)
 if (BUILD_DOCS)
 	find_package(Doxygen REQUIRED)
 endif ()
