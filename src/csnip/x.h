f5c1328900d9cca3d89aaf621d42a1f774746ae5
Lorenz Minder <lorenz@bitripple.com>
2022-10-24 23:30:21 -0500

    x:  Order declarations & macros alphabetically.
    The x API is suspectible to grow over time, and has lots of unrelated
    functions.  As such, it is easier to keep an alphabetical ordering; as
    there is not another natural one.

--- src/csnip/x.h
+++ src/csnip/x.h
@@ -31,6 +31,17 @@
 
 #include <csnip/cext.h>
 
+/* Mark symbol for export.
+ *
+ * XXX this should not be specific to x.h; other modules could use
+ * this just as well.  Currently they don't, however.
+ */
+#ifdef csnip_EXPORTS
+#  define CSNIP_SYMBOL	csnip_cext_export
+#else
+#  define CSNIP_SYMBOL	csnip_cext_import
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -54,16 +65,6 @@ extern "C" {
 #endif
 
 
-/**	Portable strerror_r().
- *
- *	This implements the POSIX conforming version of strerror_r().
- *	On Unix, the libc strerror_r() is used without.  There is no
- *	need in user code to define the feature macros that are needed
- *	to access the system strerror_r().  On Windows, strerror_s() is
- *	used in the backend.
- */
-int csnip_x_strerror_r(int errnum, char* buf, size_t buflen);
-
 /**	Wrapper for asprintf() or csnip_x_asprintf_imp().  */
 #define csnip_x_asprintf asprintf
 #ifndef CSNIP_CONF__HAVE_ASPRINTF
@@ -75,82 +76,6 @@ int asprintf(char** strp, const char* fmt, ...);
 
 /**	Portable asprintf().  */
 int csnip_x_asprintf_imp(char** strp, const char* format, ...);
-
-/**	Wrapper for vasprintf() or csnip_x_vasprintf() */
-#define csnip_x_vasprintf vasprintf
-#ifndef CSNIP_CONF__HAVE_VASPRINTF
-#undef csnip_x_vasprintf
-#define csnip_x_vasprintf csnip_x_vasprintf_imp
-#else
-int vasprintf(char** strp, const char* fmt, va_list ap);
-#endif
-
-/**	Portable vasprintf().
- *
- *	@sa csnip_x_asprintf_imp().
- */
-int csnip_x_vasprintf_imp(char** strp, const char* format, va_list ap);
-
-/**	Portable strdup(). */
-char* csnip_x_strdup(const char* s);
-
-/**	Portable struct iovec.
- *
- *	When the system has it, we just alias our iovec to the system's.
- *	Otherwise we roll our own.
- */
-#if defined(CSNIP_CONF__HAVE_READV) || defined(CSNIP_CONF__HAVE_WRITEV)
-#  define csnip_x_iovec	iovec
-#else
-struct csnip_x_iovec {
-	void  *iov_base;
-	size_t iov_len;
-};
-#endif
-
-/**	Wrapper for writev() or csnip_x_writev().
- *
- *	On systems that have writev(), that system call is used.
- *	Otherwise, the less efficient csnip_x_writev_imp() is used.
- */
-#define csnip_x_writev writev
-#if !defined(CSNIP_CONF__HAVE_WRITEV)
-#undef csnip_x_writev
-#define csnip_x_writev csnip_x_writev_imp
-#endif
-
-/**	Csnip's own writev().
- *
- *	Behaves like writev(), though isn't as efficient.
- *
- *	This implementation first copies the scattered blocks into a
- *	temporary buffer and then calls write().  If the write() system
- *	call respects POSIX' atomicity guarantees, then so will this
- *	implementation of readv().
- */
-csnip_x_ssize_t csnip_x_writev_imp(int fd,
-			const struct csnip_x_iovec* iov,
-			int iovcnt);
-
-/**	Wrapper for readv() or csnip_x_readv_imp().
- *
- *	Maps to readv() on systems that have it, and to
- *	csnip_x_readv_imp() otherwise.
- */
-#define csnip_x_readv readv
-#if !defined(CSNIP_CONF__HAVE_READV)
-#undef csnip_x_readv
-#define csnip_x_readv csnip_x_readv_imp
-#endif
-
-/**	Csnip's own readv().
- *
- *	Reads into a temporary buffer using read(), then copies into the
- *	scattered blocks.
- */
-csnip_x_ssize_t csnip_x_readv_imp(int fd,
-			const struct csnip_x_iovec* iov,
-			int iovcnt);
 
 /**	Wrapper for getdelim or csnip_x_getdelim_imp() */
 #define csnip_x_getdelim getdelim
@@ -187,11 +112,18 @@ csnip_x_ssize_t csnip_x_getline_imp(char** lineptr,
 /**	Csnip's own getopt() */
 int csnip_x_getopt_imp(int argc, char* argv[], const char* optstring);
 
-/**	optind */
-#define csnip_x_optind optind
-#if !defined(CSNIP_CONF__HAVE_GETOPT)
-#undef csnip_x_optind
-#define csnip_x_optind csnip_x_optind_imp
+/**	Portable struct iovec.
+ *
+ *	When the system has it, we just alias our iovec to the system's.
+ *	Otherwise we roll our own.
+ */
+#if defined(CSNIP_CONF__HAVE_READV) || defined(CSNIP_CONF__HAVE_WRITEV)
+#  define csnip_x_iovec	iovec
+#else
+struct csnip_x_iovec {
+	void  *iov_base;
+	size_t iov_len;
+};
 #endif
 
 /**	optarg */
@@ -200,13 +132,7 @@ int csnip_x_getopt_imp(int argc, char* argv[], const char* optstring);
 #undef csnip_x_optarg
 #define csnip_x_optarg csnip_x_optarg_imp
 #endif
-
-/**	optopt */
-#define csnip_x_optopt optopt
-#if !defined(CSNIP_CONF__HAVE_GETOPT)
-#undef csnip_x_optopt
-#define csnip_x_optopt csnip_x_optopt_imp
-#endif
+CSNIP_SYMBOL extern char* csnip_x_optarg_imp;
 
 /**	opterr */
 #define csnip_x_opterr opterr
@@ -214,17 +140,56 @@ int csnip_x_getopt_imp(int argc, char* argv[], const char* optstring);
 #undef csnip_x_opterr
 #define csnip_x_opterr csnip_x_opterr_imp
 #endif
+CSNIP_SYMBOL extern int csnip_x_opterr_imp;
 
-#ifdef csnip_EXPORTS
-#  define CSNIP_SYMBOL	csnip_cext_export
-#else
-#  define CSNIP_SYMBOL	csnip_cext_import
+/**	optind */
+#define csnip_x_optind optind
+#if !defined(CSNIP_CONF__HAVE_GETOPT)
+#undef csnip_x_optind
+#define csnip_x_optind csnip_x_optind_imp
+#endif
+CSNIP_SYMBOL extern int csnip_x_optind_imp;
+
+/**	optopt */
+#define csnip_x_optopt optopt
+#if !defined(CSNIP_CONF__HAVE_GETOPT)
+#undef csnip_x_optopt
+#define csnip_x_optopt csnip_x_optopt_imp
+#endif
+CSNIP_SYMBOL extern int csnip_x_optopt_imp;
+
+/**	Wrapper for readv() or csnip_x_readv_imp().
+ *
+ *	Maps to readv() on systems that have it, and to
+ *	csnip_x_readv_imp() otherwise.
+ */
+#define csnip_x_readv readv
+#if !defined(CSNIP_CONF__HAVE_READV)
+#undef csnip_x_readv
+#define csnip_x_readv csnip_x_readv_imp
 #endif
 
-CSNIP_SYMBOL extern char* csnip_x_optarg_imp;
-CSNIP_SYMBOL extern int csnip_x_optind_imp;
-CSNIP_SYMBOL extern int csnip_x_optopt_imp;
-CSNIP_SYMBOL extern int csnip_x_opterr_imp;
+/**	Csnip's own readv().
+ *
+ *	Reads into a temporary buffer using read(), then copies into the
+ *	scattered blocks.
+ */
+csnip_x_ssize_t csnip_x_readv_imp(int fd,
+			const struct csnip_x_iovec* iov,
+			int iovcnt);
+
+/**	Portable strdup(). */
+char* csnip_x_strdup(const char* s);
+
+/**	Portable strerror_r().
+ *
+ *	This implements the POSIX conforming version of strerror_r().
+ *	On Unix, the libc strerror_r() is used without.  There is no
+ *	need in user code to define the feature macros that are needed
+ *	to access the system strerror_r().  On Windows, strerror_s() is
+ *	used in the backend.
+ */
+int csnip_x_strerror_r(int errnum, char* buf, size_t buflen);
 
 /**	Wrapper for strtok_r() or csnip_x_strtok_r() */
 #define csnip_x_strtok_r strtok_r
@@ -236,6 +201,45 @@ CSNIP_SYMBOL extern int csnip_x_opterr_imp;
 /**	Csnip's own strtok_r() */
 char* csnip_x_strtok_r_imp(char* str, const char* delim, char** saveptr);
 
+/**	Wrapper for vasprintf() or csnip_x_vasprintf() */
+#define csnip_x_vasprintf vasprintf
+#ifndef CSNIP_CONF__HAVE_VASPRINTF
+#undef csnip_x_vasprintf
+#define csnip_x_vasprintf csnip_x_vasprintf_imp
+#else
+int vasprintf(char** strp, const char* fmt, va_list ap);
+#endif
+
+/**	Portable vasprintf().
+ *
+ *	@sa csnip_x_asprintf_imp().
+ */
+int csnip_x_vasprintf_imp(char** strp, const char* format, va_list ap);
+
+/**	Wrapper for writev() or csnip_x_writev().
+ *
+ *	On systems that have writev(), that system call is used.
+ *	Otherwise, the less efficient csnip_x_writev_imp() is used.
+ */
+#define csnip_x_writev writev
+#if !defined(CSNIP_CONF__HAVE_WRITEV)
+#undef csnip_x_writev
+#define csnip_x_writev csnip_x_writev_imp
+#endif
+
+/**	Csnip's own writev().
+ *
+ *	Behaves like writev(), though isn't as efficient.
+ *
+ *	This implementation first copies the scattered blocks into a
+ *	temporary buffer and then calls write().  If the write() system
+ *	call respects POSIX' atomicity guarantees, then so will this
+ *	implementation of readv().
+ */
+csnip_x_ssize_t csnip_x_writev_imp(int fd,
+			const struct csnip_x_iovec* iov,
+			int iovcnt);
+
 /** @} */
 
 #ifdef __cplusplus
@@ -245,33 +249,37 @@ char* csnip_x_strtok_r_imp(char* str, const char* delim, char** saveptr);
 #endif /* CSNIP_X_H */
 
 #if defined(CSNIP_SHORT_NAMES) && !defined(CSNIP_X_HAVE_SHORT_NAMES)
+
+/* types */
 #define x_ssize_t			csnip_x_ssize_t
-#define x_iovec				csnip_x_iovec
-#define x_strerror_r			csnip_x_strerror_r
+
+/* API calls */
 #define x_asprintf			csnip_x_asprintf
 #define x_asprintf_imp			csnip_x_asprintf_imp
-#define x_vasprintf			csnip_x_vasprintf
-#define x_vasprintf_imp			csnip_x_vasprintf_imp
-#define x_strdup			csnip_x_strdup
-#define x_writev			csnip_x_writev
-#define x_writev_imp			csnip_x_writev_imp
-#define x_readv				csnip_x_readv
-#define x_readv_imp			csnip_x_readv_imp
 #define x_getdelim			csnip_x_getdelim
 #define x_getdelim_imp			csnip_x_getdelim_imp
 #define x_getline			csnip_x_getline
 #define x_getline_imp			csnip_x_getline_imp
 #define x_getopt			csnip_x_getopt
 #define x_getopt_imp			csnip_x_getopt_imp
-#define x_optind			csnip_x_optind
-#define x_optind_imp			csnip_x_optind_imp
+#define x_iovec				csnip_x_iovec
 #define x_optarg			csnip_x_optarg
 #define x_optarg_imp			csnip_x_optarg_imp
-#define x_optopt			csnip_x_optopt
-#define x_optopt_imp			csnip_x_optopt_imp
 #define x_opterr			csnip_x_opterr
 #define x_opterr_imp			csnip_x_opterr_imp
+#define x_optind			csnip_x_optind
+#define x_optind_imp			csnip_x_optind_imp
+#define x_optopt			csnip_x_optopt
+#define x_optopt_imp			csnip_x_optopt_imp
+#define x_readv				csnip_x_readv
+#define x_readv_imp			csnip_x_readv_imp
+#define x_strdup			csnip_x_strdup
+#define x_strerror_r			csnip_x_strerror_r
 #define x_strtok_r			csnip_x_strtok_r
 #define x_strtok_r_imp			csnip_x_strtok_r_imp
+#define x_vasprintf			csnip_x_vasprintf
+#define x_vasprintf_imp			csnip_x_vasprintf_imp
+#define x_writev			csnip_x_writev
+#define x_writev_imp			csnip_x_writev_imp
 #define CSNIP_X_HAVE_SHORT_NAMES
 #endif /* CSNIP_SHORT_NAMES && !CSNIP_X_HAVE_SHORT_NAMES */
