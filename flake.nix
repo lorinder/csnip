147961486844912fb19bd51786174c804b2c77f4
Lorenz Minder <lminder@gmx.net>
2024-01-13 18:08:39 -0600

    build:  Add nix flake.
    For easy integration in with projects using Nix.  This is experimental
    at this point.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "C snippet library";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      packages = {
+        default = pkgs.stdenv.mkDerivation {
+          name = "csnip";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+          ];
+
+          buildInputs = with pkgs; [ ];
+        };
+      };
+    });
+}
+
+# vim:sw=2:sts=2:et
