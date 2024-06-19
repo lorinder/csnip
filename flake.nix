7d5860e36bbe59d5253bce9e11039a74ba54cbab
Lorenz Minder <lminder@gmx.net>
2024-06-19 11:58:21 -0500

    flake:  Move to NixOS 24.05.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "C snippet library";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
